2502e89034249603655ac2ada3224ab565055609
Aliaschraf <ali-karimov@gmx.de>
2026-04-05 18:05:59 +0200

    modified:   server/TaskManager.Api/bin/Debug/net9.0/TaskManager.Api.dll 	modified:   server/TaskManager.Api/bin/Debug/net9.0/TaskManager.Api.exe 	modified:   server/TaskManager.Api/bin/Debug/net9.0/TaskManager.Api.pdb 	modified:   server/TaskManager.Api/obj/Debug/net9.0/TaskManager.Api.AssemblyInfo.cs 	modified:   server/TaskManager.Api/obj/Debug/net9.0/TaskManager.Api.AssemblyInfoInputs.cache 	modified:   server/TaskManager.Api/obj/Debug/net9.0/TaskManager.Api.dll 	modified:   server/TaskManager.Api/obj/Debug/net9.0/TaskManager.Api.pdb 	modified:   server/TaskManager.Api/obj/Debug/net9.0/TaskManager.Api.sourcelink.json 	modified:   server/TaskManager.Api/obj/Debug/net9.0/apphost.exe 	modified:   server/TaskManager.Api/obj/Debug/net9.0/ref/TaskManager.Api.dll 	modified:   server/TaskManager.Api/obj/Debug/net9.0/refint/TaskManager.Api.dll 	modified:   server/TaskManager.Api/obj/Debug/net9.0/rjsmcshtml.dswa.cache.json 	modified:   server/TaskManager.Api/obj/Debug/net9.0/rjsmrazor.dswa.cache.json

--- server/TaskManager.Api/obj/Debug/net9.0/TaskManager.Api.AssemblyInfo.cs
+++ server/TaskManager.Api/obj/Debug/net9.0/TaskManager.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManager.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d48784ccb2819a9f480217f8ccf9cbe885c84baa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+991e0863193396038ce4fb0fd97614fa586e86ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManager.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManager.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
